41ee0f05d899f456c7e03ef31c39aa96e3aeb3b4
Abderahim <ita27rmp100@gmail.com>
2025-03-15 06:34:17 +0100

    delete useless ones

--- speed.py
+++
@@ -1,8 +0,0 @@
-import speedtest 
-try :
-    l = speedtest.Speedtest()
-    download = f'{l.download()/1048576:03.2f} Mbps'
-    upload = f'{l.upload()/1048576:03.2f} Mbps'
-    ping = f'{l.results.ping:03.2f} Mbps'
-except speedtest.ConfigRetrievalError :
-    print("there is  no Internet")
